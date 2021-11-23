6367a096a9a7b4c8660ec421b76c566a9e1bc306
Tanvir Mohammad Rizvy <tanvirmohammad2@gmail.com>
2021-11-23 14:42:04 +0600

    work

---
+++ src/components/Announcement.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+    height: 30px;
+    background-color: teal;
+    color: white;
+`;
+
+const Announcement = () => {
+    return (
+        <Container>
+            
+        </Container>
+    )
+}
+
+export default Announcement
